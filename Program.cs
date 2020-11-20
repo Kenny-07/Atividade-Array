e252500954f69ef3eefc379ba7846c925eb648a0
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-11-20 09:41:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
