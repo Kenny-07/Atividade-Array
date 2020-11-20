4a894511cd13eabf90c75a49f8a20c5ce75575fe
Alan da Costa Santos <alancostasantos07@gmail.com>
2020-11-20 10:11:25 -0300

    Atividade Array Finalizada

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace Atividade_Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string bolo;
+            string acompanhamento;
+
+            string[] produtos = {"Bolo de Chocolate", "Bolo de Abacaxi", "Bolo de Baunilha", "Bolo de Cenoura", "Bolo de Laranja"};
+            string[] recheios = {"Brigadeiro", "Beijinho", "Frutas Vermelhas", "Mousse"};
+
+            Console.WriteLine("================================");
+            Console.WriteLine("Bem vindo a Confeitaria do Back!");
+            Console.WriteLine("================================");
+
+            Console.WriteLine("Qual Bolo você vai querer hoje ?");
+            Console.WriteLine("================================");
+
+            for (int i = 0; i < produtos.Length; i++)
+            {
+                Console.WriteLine(produtos[i]);
+            }
+
+            bolo = Console.ReadLine();
+
+            Console.WriteLine("=================================================================");
+            Console.WriteLine($"Para acompanhar seu {bolo} qual Recheio você deseja ?");
+            Console.WriteLine("=================================================================");
+
+            for (int i = 0; i < recheios.Length; i++)
+            {
+                Console.WriteLine(recheios[i]);
+            }
+
+            acompanhamento = Console.ReadLine();
+
+            Console.WriteLine("============================================================================");
+            Console.WriteLine($"Aproveite seu {bolo} com Recheio de {acompanhamento} !! <3");
+            Console.WriteLine("============================================================================");
         }
     }
 }
